a4aee9de652ed8e1e9d0550e86d08292a8d5cc02
= <=>
2025-09-27 23:31:15 +0700

    Remove Backend

--- backend/dist/middleware/auth.d.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export interface AuthRequest extends Request {
-    userId?: number;
-}
-export declare const authenticateToken: (req: AuthRequest, res: Response, next: NextFunction) => void;
-//# sourceMappingURL=auth.d.ts.map
--- backend/dist/utils/jwt.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const generateToken: (userId: number) => string;
-export declare const verifyToken: (token: string) => {
-    userId: number;
-};
-export declare const generateResetToken: () => string;
-//# sourceMappingURL=jwt.d.ts.map
